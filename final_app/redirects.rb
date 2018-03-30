986d79f0fa78063c94b189b4b53850326a399d70
Landon Jarzynski <ljarzynski@pivotal.io>
2018-03-29 22:41:47 -0700

    adds bot book

---
+++ final_app/redirects.rb
@@ -0,0 +1,13 @@
+# Metrics redirects, change version at each release. Have to do it this way
+# because edge.
+
+r301 '/pcf-metrics/', '/pcf-metrics/1-4/index.html'
+r301 '/pcf-metrics/index.html', '/pcf-metrics/1-4/index.html'
+r301 '/pcf-metrics/installing.html', '/pcf-metrics/1-4/installing.html'
+r301 '/pcf-metrics/sizing.html', '/pcf-metrics/1-4/sizing.html'
+r301 '/pcf-metrics/using.html', '/pcf-metrics/1-4/using.html'
+r301 '/pcf-metrics/architecture.html', '/pcf-metrics/1-4/architecture.html'
+r301 '/pcf-metrics/rn-ki.html', '/pcf-metrics/1-4/rn-ki.html'
+r301 '/pcf-metrics/troubleshooting.html', '/pcf-metrics/1-4/troubleshooting.html'
+
+r301 '/pcf-metrics/1-2/installing.html#troubleshooting', '/pcf-metrics/1-2/troubleshooting.html'
---
+++ redirects.rb
@@ -0,0 +1,2 @@
+# Metrics redirects, change version at each release. Have to do it this way
+# because edge.
